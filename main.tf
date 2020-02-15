5732d94b4aa3eb7142d72d0f1b4ee4012c042edc
Peter <pesegal@gmail.com>
2020-02-14 18:26:44 -0800

    TUAR: Chapter 2 Init

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-01ed306a12b7d1c96"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
