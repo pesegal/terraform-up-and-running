c4312c61beb6d9be112e0c5d42f70d9df827f8bb
Peter Segal <pesegal@gmail.com>
2020-08-19 17:19:29 -0700

    basic hello world server

--- main.tf
+++ main.tf
@@ -3,10 +3,29 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-01ed306a12b7d1c96"
-  instance_type = "t2.nano"
+  ami = "ami-0ba8629bff503c084"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
 }
