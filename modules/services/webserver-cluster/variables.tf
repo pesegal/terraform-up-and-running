01a0f73522430a62731872bdfef01e3e455a8ce0
Peter Segal <pesegal@gmail.com>
2020-08-31 19:25:50 -0700

    adding modules

---
+++ modules/services/webserver-cluster/variables.tf
@@ -0,0 +1,28 @@
+variable "server_port" {
+  description = "The port number the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+variable "cluster_name" {
+  description = "The name to use for all of the cluster resources"
+  type = string
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the database's remote state"
+  type = string
+}
+
+variable "db_remote_state_key" {
+  description = "The path for the database's remote state in S3"
+  type = string
+}
